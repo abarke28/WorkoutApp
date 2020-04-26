efb1633d68df71d7b7ad8cceb62b784ea75fd938
Alex Barker <abarke28@gmail.com>
2020-04-26 10:43:43 -0700

    adding tests for DropHandler

---
+++ WorkoutAppTests/ViewModel/MainVMTests.cs
@@ -0,0 +1,22 @@
+﻿using GongSolutions.Wpf.DragDrop;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WorkoutApp.Model;
+using WorkoutApp.ViewModel;
+using Xunit;
+
+namespace WorkoutAppTests.ViewModel
+{
+    public class MainVMTests
+    {
+        [Fact]
+        public void Drop_IndexGreaterThanCount_RejectsDrop()
+        {
+            var vm = new MainVM();
+            var dropInfo = new Mock<IDropInfo>();
+            dropInfo.Setup(d => d.Data).Returns(new Exercise { Description = "abc", ExerciseName = "def", ExerciseType = ExerciseType.Core });
+        }
+    }
+}
