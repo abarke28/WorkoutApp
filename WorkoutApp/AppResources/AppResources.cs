f667053220bb859a28f217f0c87a4f708d256203
Alex Barker <abarke28@gmail.com>
2020-04-28 09:53:16 -0700

    Changed AppResourcesLocation

--- WorkoutApp/AppResources/AppResources.cs
+++ WorkoutApp/AppResources/AppResources.cs
@@ -6,23 +6,14 @@ namespace WorkoutApp.AppResources
 {
     public static class AppResources
     {
-        //public static string PLAYIMAGE { get; } = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData) + @"\Poor Yorrick Workouts\Plan.png";
-        //public static string PAUSEIMAGE { get; } = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData) + @"\Poor Yorrick Workouts\Pause.png";
-        //public static string REST { get; } = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData) + @"\Poor Yorrick Workouts\rest.wav";
-        //public static string ONE { get; } = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData) + @"\Poor Yorrick Workouts\one.wav";
-        //public static string TWO { get; } = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData) + @"\Poor Yorrick Workouts\two.wav";
-        //public static string THREE { get; } = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData) + @"\Poor Yorrick Workouts\three.wav";
-        //public static string GO { get; } = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData) + @"\Poor Yorrick Workouts\go.wav";
-        //public static string DONE { get; } = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData) + @"\Poor Yorrick Workouts\done.wav";
-        //public static string GETREADY { get; } = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData) + @"\Poor Yorrick Workouts\getready.wav";
-        public const string PLAYIMAGE = @"C:\Users\Owner\source\repos\WorkoutApp\WorkoutApp\AppResources\Play.png";
-        public const string PAUSEIMAGE = @"C:\Users\Owner\source\repos\WorkoutApp\WorkoutApp\AppResources\Pause.png";
-        public const string REST = @"C:\Users\Owner\source\repos\WorkoutApp\WorkoutApp\AppResources\rest.wav";
-        public const string ONE = @"C:\Users\Owner\source\repos\WorkoutApp\WorkoutApp\AppResources\one.wav";
-        public const string TWO = @"C:\Users\Owner\source\repos\WorkoutApp\WorkoutApp\AppResources\two.wav";
-        public const string THREE = @"C:\Users\Owner\source\repos\WorkoutApp\WorkoutApp\AppResources\three.wav";
-        public const string GO = @"C:\Users\Owner\source\repos\WorkoutApp\WorkoutApp\AppResources\go.wav";
-        public const string DONE = @"C:\Users\Owner\source\repos\WorkoutApp\WorkoutApp\AppResources\done.wav";
-        public const string GETREADY = @"C:\Users\Owner\source\repos\WorkoutApp\WorkoutApp\AppResources\getready.wav";
+        public static string PLAYIMAGE { get; } = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData) + @"\Poor Yorrick Workouts\Play.png";
+        public static string PAUSEIMAGE { get; } = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData) + @"\Poor Yorrick Workouts\Pause.png";
+        public static string REST { get; } = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData) + @"\Poor Yorrick Workouts\rest.wav";
+        public static string ONE { get; } = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData) + @"\Poor Yorrick Workouts\one.wav";
+        public static string TWO { get; } = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData) + @"\Poor Yorrick Workouts\two.wav";
+        public static string THREE { get; } = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData) + @"\Poor Yorrick Workouts\three.wav";
+        public static string GO { get; } = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData) + @"\Poor Yorrick Workouts\go.wav";
+        public static string DONE { get; } = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData) + @"\Poor Yorrick Workouts\done.wav";
+        public static string GETREADY { get; } = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData) + @"\Poor Yorrick Workouts\getready.wav";
     }
 }
