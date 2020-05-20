becf6c8484423a76cb8899228f180ae42dd57e1a
Alex Barker <abarke28@gmail.com>
2020-05-19 17:10:24 -0700

    added dp for selecting all text when element is selected

--- WorkoutApp/utils/DialogCloser.cs
+++ WorkoutApp/utils/DialogCloser.cs
@@ -8,8 +8,9 @@ namespace WorkoutApp.utils
     public static class DialogCloser
     {
         public static readonly DependencyProperty DialogResultProperty =
-            DependencyProperty.RegisterAttached("DialogResult", typeof(bool?), 
-                typeof(DialogCloser), new PropertyMetadata(DialogResultChanged));
+            DependencyProperty.RegisterAttached("DialogResult",
+                typeof(bool?), typeof(DialogCloser), 
+                new PropertyMetadata(DialogResultChanged));
 
         private static void DialogResultChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
---
+++ WorkoutApp/utils/TextFocuser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WorkoutApp.utils
+{
+    public static class TextFocuser
+    {
+        public static readonly DependencyProperty SelectAllOnFocusProperty =
+            DependencyProperty.Register("SelectAllOnFocus", 
+                typeof(bool?), typeof(TextFocuser), 
+                new PropertyMetadata(false, SelectAllOnFocusChanged));
+         
+        private static void SelectAllOnFocusChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is TextBox textBox)
+            {
+                if (e.NewValue == e.OldValue) return;
+
+                if ((bool)e.NewValue) textBox.GotFocus += SelectAll;
+                else textBox.GotFocus -= SelectAll;
+            }
+        }
+
+        public static void SelectAll(object sender, RoutedEventArgs e)
+        {
+            if (e.OriginalSource is TextBox textBox)
+            {
+                textBox.SelectAll();
+            }
+        }
+    }
+}
